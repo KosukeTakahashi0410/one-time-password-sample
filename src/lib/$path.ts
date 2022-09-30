640066556bb66cbb1e11a8b2f1b7178ddd0e9484
takahashikosuke <takohashi0410@gmail.com>
2022-10-01 00:27:00 +0900

    add:pathpidaで生成されたパスを追加

---
+++ src/lib/$path.ts
@@ -0,0 +1,14 @@
+export const pagesPath = {
+  "otp": {
+    "complete": {
+      $url: (url?: { hash?: string }) => ({ pathname: '/otp/complete' as const, hash: url?.hash })
+    },
+    "request": {
+      $url: (url?: { hash?: string }) => ({ pathname: '/otp/request' as const, hash: url?.hash })
+    },
+    $url: (url?: { hash?: string }) => ({ pathname: '/otp' as const, hash: url?.hash })
+  },
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
+}
+
+export type PagesPath = typeof pagesPath
