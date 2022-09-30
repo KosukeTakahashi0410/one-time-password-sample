406fba90df1007bc5c81879741db3e56ae8239d6
takahashikosuke <takohashi0410@gmail.com>
2022-09-30 23:36:53 +0900

    add:eslintを追加

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json']
+  },
+  plugins: ['react'],
+  rules: {
+    // MEMO: eslint-plugin-reactと競合するのでOFF
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
