7dfd214e35afcfb63d1e2e5232131bda20b2ad0c
takahashikosuke <takohashi0410@gmail.com>
2022-10-01 17:13:05 +0900

    fix:otp送信メソッドを追加

--- src/pages/otp/index.tsx
+++ src/pages/otp/index.tsx
@@ -1,37 +1,58 @@
 import type { NextPage } from "next";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { otpFormScheme } from "@/validation/formSchemes";
+import { pagesPath } from "@/lib/$path";
+import { useRouter } from "next/router";
 
-interface PhoneInput {
-  phoneNumber: string;
+interface OTPInput {
+  otp: string;
 }
 
 const OTP: NextPage = () => {
+  const router = useRouter();
   const {
     register,
+    handleSubmit,
     formState: { isValid, isSubmitting, errors },
-  } = useForm<PhoneInput>({
+  } = useForm<OTPInput>({
     mode: "onBlur",
     resolver: yupResolver(otpFormScheme),
   });
 
+  /**
+   * ワンタイムパスワードを送信する
+   * @param inputValue フォームで入力した値
+   */
+  const submitOTP: SubmitHandler<OTPInput> = async ({ otp }): Promise<void> => {
+    try {
+      // TODO ここでAPIを呼び出し
+      console.log(otp);
+
+      // 入力画面に遷移
+      await router.replace(pagesPath.otp.$url());
+    } catch (error) {
+      // TODO ここでtoastとか出すか
+    }
+  };
+
   return (
     <div>
-      <h1>トップ</h1>
+      <h1>ワンタイムパスワードを入力する</h1>
       <section>
-        {/* FIXME: action */}
         <form>
           <div>
-            <label htmlFor="">携帯電話番号</label>
+            <label onSubmit={handleSubmit(submitOTP)}>携帯電話番号</label>
             <input
-              {...register("phoneNumber")}
+              {...register("otp")}
               placeholder="半角英数字で入力してください"
             />
             <p>例）08000000000</p>
-            {errors.phoneNumber != null && <p>{errors.phoneNumber.message}</p>}
+            {errors.otp != null && <p>{errors.otp.message}</p>}
           </div>
-          <button disabled={!isValid || isSubmitting}>送信</button>
+          <button type="submit" disabled={!isValid || isSubmitting}>
+            送信
+          </button>
         </form>
       </section>
     </div>
