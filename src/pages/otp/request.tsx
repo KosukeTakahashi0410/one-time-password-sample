16d1cfb779ef6ac89f94b46b2adbd220945d3a19
takahashikosuke <takohashi0410@gmail.com>
2022-10-01 15:47:09 +0900

    fix:2段階認証SNS送信処理をざっくり作成

--- src/pages/otp/request.tsx
+++ src/pages/otp/request.tsx
@@ -1,27 +1,48 @@
 import type { NextPage } from "next";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { phoneNumberFormScheme } from "@/validation/formSchemes";
+import { useRouter } from "next/router";
+import { pagesPath } from "@/lib/$path";
 
 interface PhoneInput {
   phoneNumber: string;
 }
 
 const OTPRequest: NextPage = () => {
+  const router = useRouter();
   const {
     register,
+    handleSubmit,
     formState: { isValid, isSubmitting, errors },
   } = useForm<PhoneInput>({
     mode: "onBlur",
     resolver: yupResolver(phoneNumberFormScheme),
   });
 
+  /**
+   * 電話を送信する
+   * @param inputValue フォームで入力した値
+   */
+  const submitPhone: SubmitHandler<PhoneInput> = async ({
+    phoneNumber,
+  }): Promise<void> => {
+    try {
+      // TODO ここでAPIを呼び出し
+      console.log(phoneNumber);
+
+      // 入力画面に遷移
+      await router.push(pagesPath.otp.$url());
+    } catch (error) {
+      // TODO ここでtoastとか出すか
+    }
+  };
+
   return (
     <div>
-      <h1>トップ</h1>
+      <h1>2段階認証を行う</h1>
       <section>
-        {/* FIXME: action */}
-        <form>
+        <form onSubmit={handleSubmit(submitPhone)}>
           <div>
             <label htmlFor="">携帯電話番号</label>
             <input
@@ -31,7 +52,7 @@ const OTPRequest: NextPage = () => {
             <p>例）08000000000</p>
             {errors.phoneNumber != null && <p>{errors.phoneNumber.message}</p>}
           </div>
-          <button disabled={!isValid || isSubmitting}>送信</button>
+          <button disabled={!isValid || isSubmitting}>SNSを送信</button>
         </form>
       </section>
     </div>
