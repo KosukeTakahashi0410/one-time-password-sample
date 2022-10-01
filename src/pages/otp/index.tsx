25aa3f568110f0c29f2bda8baa50078323b52d69
takahashikosuke <takohashi0410@gmail.com>
2022-10-01 17:40:01 +0900

    add:otp credential 処理

--- src/pages/otp/index.tsx
+++ src/pages/otp/index.tsx
@@ -4,6 +4,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { otpFormScheme } from "@/validation/formSchemes";
 import { pagesPath } from "@/lib/$path";
 import { useRouter } from "next/router";
+import { useCallback, useEffect } from "react";
 
 interface OTPInput {
   otp: string;
@@ -14,6 +15,9 @@ const OTP: NextPage = () => {
   const {
     register,
     handleSubmit,
+    setValue,
+    getValues,
+    trigger,
     formState: { isValid, isSubmitting, errors },
   } = useForm<OTPInput>({
     mode: "onBlur",
@@ -24,17 +28,68 @@ const OTP: NextPage = () => {
    * ワンタイムパスワードを送信する
    * @param inputValue フォームで入力した値
    */
-  const submitOTP: SubmitHandler<OTPInput> = async ({ otp }): Promise<void> => {
-    try {
-      // TODO ここでAPIを呼び出し
-      console.log(otp);
+  const submitOTP: SubmitHandler<OTPInput> = useCallback(
+    async ({ otp }): Promise<void> => {
+      try {
+        // TODO ここでAPIを呼び出し
+        console.log(otp);
 
-      // 入力画面に遷移
-      await router.replace(pagesPath.otp.$url());
-    } catch (error) {
-      // TODO ここでtoastとか出すか
+        // 入力画面に遷移
+        await router.replace(pagesPath.otp.$url());
+      } catch (error) {
+        // TODO ここでtoastとか出すか
+      }
+    },
+    [router]
+  );
+
+  useEffect(() => {
+    // OTP Credential APIが使用可能かどうか
+    if (!("OTPCredential" in window)) {
+      return;
     }
-  };
+
+    const input = document.querySelector('input[autocomplete="one-time-code"]');
+
+    if (input == null) {
+      return;
+    }
+
+    const form = input.closest("form");
+    const ac = new AbortController();
+    const abort = (): void => ac.abort();
+
+    if (form != null) {
+      form.addEventListener("submit", abort);
+    }
+
+    // OTP処理
+    navigator.credentials
+      .get({
+        // @ts-expect-error
+        otp: { transport: ["sms"] },
+        signal: ac.signal,
+      })
+      .then(async (otp) => {
+        // @ts-expect-error
+        setValue("otp", otp?.code ?? "");
+        await trigger("otp");
+
+        if (isValid) {
+          submitOTP({ otp: getValues("otp") });
+        }
+      })
+      .catch((error) => {
+        // 本来ならばここでできることはないが、便宜上エラーログを出すように
+        console.log(error);
+      });
+
+    return () => {
+      if (form != null) {
+        form?.removeEventListener("submit", abort);
+      }
+    };
+  }, [getValues, isValid, setValue, submitOTP, trigger]);
 
   return (
     <div>
@@ -46,6 +101,7 @@ const OTP: NextPage = () => {
             <input
               {...register("otp")}
               placeholder="半角英数字で入力してください"
+              autoComplete="one-time-code"
             />
             <p>例）08000000000</p>
             {errors.otp != null && <p>{errors.otp.message}</p>}
