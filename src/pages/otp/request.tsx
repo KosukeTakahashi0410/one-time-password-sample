e7307c1440b2cc866b14ae41dafbc81ab890d704
takahashikosuke <takahashikosuke@team-lab.com>
2022-10-03 22:06:21 +0900

    fix:一旦雑にcss

--- src/pages/otp/request.tsx
+++ src/pages/otp/request.tsx
@@ -39,22 +39,39 @@ const OTPRequest: NextPage = () => {
   };
 
   return (
-    <div>
-      <h1>2段階認証を行う</h1>
-      <section>
-        <form onSubmit={handleSubmit(submitPhone)}>
-          <div>
-            <label htmlFor="">携帯電話番号</label>
-            <input
-              {...register("phoneNumber")}
-              placeholder="半角英数字で入力してください"
-            />
-            <p>例）08000000000</p>
-            {errors.phoneNumber != null && <p>{errors.phoneNumber.message}</p>}
-          </div>
-          <button disabled={!isValid || isSubmitting}>SNSを送信</button>
-        </form>
-      </section>
+    <div className="flex-col justify-center max-w-full my-6">
+      <div className="max-w-screen-md mx-auto">
+        <h1>2段階認証を行う</h1>
+        <section>
+          <form onSubmit={handleSubmit(submitPhone)}>
+            <div className="flex-col  justify-center">
+              <label className="form-label inline-block mb-2 text-gray-700">携帯電話番号</label>
+              <input
+                {...register("phoneNumber")}
+                placeholder="半角英数字で入力してください"
+                className="form-control
+                  block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              />
+              <p>例）08000000000</p>
+              {errors.phoneNumber != null && <p>{errors.phoneNumber.message}</p>}
+            </div>
+            <button disabled={!isValid || isSubmitting}>SNSを送信</button>
+          </form>
+        </section>
+      </div>
     </div>
   );
 };
